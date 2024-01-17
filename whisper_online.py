538939ab5fe81249aac1a181740675c4fbfbaa77
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2024-01-17 03:16:13 -0800

    Understanding the inner workings

--- whisper_online.py
+++ whisper_online.py
@@ -241,6 +241,7 @@ class OnlineASRProcessor:
         self.silence_iters = 0
 
     def insert_audio_chunk(self, audio):
+        print("We are here" , file = sys.stderr , flush=True)
         self.audio_buffer = np.append(self.audio_buffer, audio)
 
     def prompt(self):
